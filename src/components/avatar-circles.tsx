5623422c8036cc451f5c60bdc4840b476608409f
sepidsoroush <s.soroush2012@gmail.com>
2024-11-25 18:28:00 +0200

    add name and position to the avatars

--- src/components/avatar-circles.tsx
+++ src/components/avatar-circles.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { cn } from "../lib/utils";
 
 interface Avatar {
@@ -11,18 +12,35 @@ interface AvatarCirclesProps {
 }
 
 const AvatarCircles = ({ className, avatarUrls }: AvatarCirclesProps) => {
+  const [selectedAvatar, setSelectedAvatar] = useState<Avatar | null>(null);
+
   return (
-    <div className={cn("z-10 flex -space-x-3 rtl:space-x-reverse", className)}>
-      {avatarUrls.map((url, index) => (
-        <img
-          key={index}
-          className="h-16 w-16 rounded-full border-4 border-white dark:border-[#242424]"
-          src={url.imageUrl}
-          width={64}
-          height={64}
-          alt={`Avatar ${index + 1}`}
-        />
-      ))}
+    <div className="flex flex-col items-center gap-4">
+      <div
+        className={cn("z-10 flex -space-x-3 rtl:space-x-reverse", className)}
+      >
+        {avatarUrls.map((url, index) => (
+          <img
+            key={index}
+            className="h-16 w-16 rounded-full border-4 border-white dark:border-[#242424] hover:-translate-y-1 overflow-hidden"
+            src={url.imageUrl}
+            width={64}
+            height={64}
+            alt={`Avatar ${index + 1}`}
+            onMouseOver={() => setSelectedAvatar(url)}
+            onMouseOut={() => setSelectedAvatar(null)}
+          />
+        ))}
+      </div>
+      {selectedAvatar && (
+        <div className="flex flex-row rounded-full w-full p-4 border gap-2">
+          <p className="font-bold">{selectedAvatar.name}</p>
+          {"|"}
+          <p className="font-medium text-neutral-500">
+            {selectedAvatar.position}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
