92d59906c4cacfe93620d4ab4d5e250db252989b
sepidsoroush <s.soroush2012@gmail.com>
2024-11-26 10:28:02 +0200

    add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(var(--primary))",
+        "primary-foreground": "hsl(var(--primary-foreground))",
+        secondary: "hsl(var(--secondary))",
+        "secondary-foreground": "hsl(var(--secondary-foreground))",
+        accent: "hsl(var(--accent))",
+        "accent-foreground": "hsl(var(--accent-foreground))",
+        muted: "hsl(var(--muted))",
+        "muted-foreground": "hsl(var(--muted-foreground))",
+        destructive: "hsl(var(--destructive))",
+        "destructive-foreground": "hsl(var(--destructive-foreground))",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addBase }) => {
+      addBase({
+        ":root": {
+          "--tw-text-opacity": "1",
+          "--tw-bg-opacity": "1",
+        },
+      });
+    }),
+  ],
 };
