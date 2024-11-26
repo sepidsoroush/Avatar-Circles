2873f6ce1a1d68b176006b664c18b4548bf68ea7
sepidsoroush <s.soroush2012@gmail.com>
2024-11-26 09:59:58 +0200

    update Vite configuration

--- src/components/avatar-circles.tsx
+++ src/components/avatar-circles.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 
 interface Avatar {
   imageUrl: string;
