fbdfc122f1676c8e4e44638a35cbdd44c12a848f
sepidsoroush <s.soroush2012@gmail.com>
2024-12-01 18:15:46 +0200

    add Fade Text animation to texts

--- src/components/avatar-circles.tsx
+++ src/components/avatar-circles.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+import { FadeText } from "./fade-text";
 
 interface Avatar {
   imageUrl: string;
@@ -14,6 +15,18 @@ interface AvatarCirclesProps {
 
 const AvatarCircles = ({ className, avatarUrls }: AvatarCirclesProps) => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [hoveredDirection, setHoveredDirection] = useState<"left" | "right">(
+    "left"
+  );
+
+  const handleMouseEnter = (index: number, e: React.MouseEvent) => {
+    const element = e.currentTarget as HTMLElement;
+    const containerCenter =
+      element.getBoundingClientRect().left + element.offsetWidth / 2;
+    const direction = e.clientX < containerCenter ? "left" : "right";
+    setHoveredDirection(direction);
+    setHoveredIndex(index);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center gap-2 h-full">
@@ -27,7 +40,7 @@ const AvatarCircles = ({ className, avatarUrls }: AvatarCirclesProps) => {
           <motion.div
             key={index}
             className="h-16 w-16 rounded-full border-4 border-background dark:border-foreground overflow-hidden z-10"
-            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseEnter={(e) => handleMouseEnter(index, e)}
             onMouseLeave={() => setHoveredIndex(null)}
             whileHover={{
               y: -7,
@@ -48,22 +61,38 @@ const AvatarCircles = ({ className, avatarUrls }: AvatarCirclesProps) => {
           </motion.div>
         ))}
       </div>
-      {hoveredIndex !== null && (
-        <motion.div
-          className="flex flex-row rounded-full p-4 bg-accent-foreground dark:bg-accent gap-2 divide-x mt-36"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 20 }}
-          transition={{ duration: 0.2 }}
-        >
-          <p className="font-semibold text-accent dark:text-accent-foreground">
-            {avatarUrls[hoveredIndex].name}
-          </p>
-          <p className="text-muted-foreground px-2">
-            {avatarUrls[hoveredIndex].position}
-          </p>
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {hoveredIndex !== null && (
+          <motion.div
+            className="flex flex-row rounded-full p-4 bg-accent-foreground dark:bg-accent gap-2 mt-36"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.1 }}
+          >
+            <div className="font-semibold text-accent dark:text-accent-foreground">
+              <FadeText
+                key={avatarUrls[hoveredIndex].name}
+                direction={hoveredDirection}
+                framerProps={{
+                  show: { transition: { delay: 0.05 } },
+                }}
+                text={avatarUrls[hoveredIndex].name}
+              />
+            </div>
+            <div className="text-muted-foreground px-2">
+              <FadeText
+                key={avatarUrls[hoveredIndex].position}
+                direction={hoveredDirection}
+                framerProps={{
+                  show: { transition: { delay: 0.05 } },
+                }}
+                text={avatarUrls[hoveredIndex].position}
+              />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
